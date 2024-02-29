abee1c759060c6fe5bb2cf31ba8b0211002d6147
Artur-1995 <89254423508@mail.ru>
2024-02-29 13:41:09 +0300

    ДЗ к уроку 10

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-Мониторов, Евгений, Иван, Петров
-Сидоров, Иван, Петров, Петров
-Бритый, Иван, Иван, Иван
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot'] = '1'
+data.loc[data['whoAmI'] == 'robot', 'human'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human'] = '1'
+data.loc[data['whoAmI'] == 'human', 'robot'] = '0'
+data.head()
